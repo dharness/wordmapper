e30a7a31718061bc4ea63589dcec7087a0c0690d
Dylan Harness <dharness@uwo.ca>
2018-10-20 17:16:26 -0700

    .

--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
-sudo apt-get install build-essential checkinstall -y
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev  -y
-sudo add-apt-repository ppa:jonathonf/python-3.7 -y
-sudo apt-get install virtualenv -y
-virtualenv --python=python3.7 env
-. ./env/bin/activate
+yum install python3.x86_64 -y
+yum install virtualenv -y
